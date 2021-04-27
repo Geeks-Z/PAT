f7b0139b3295ee3671c6efa0ebd356d6579c002d
Geeks_Z <izhw1024@163.com>
2021-04-27 21:15:19 +0800

    1st_PAT_Basic

---
+++ PAT1024.cpp
@@ -0,0 +1,87 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: Geeks_Z
+ * @Date: 2021-04-26 21:12:23
+ * @LastEditors: Geeks_Z
+ * @LastEditTime: 2021-04-26 21:39:45
+ */
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  freopen("input.txt", "r", stdin);
+  string str, digitalSub;
+  cin >> str;
+  if (str[0] == '-')
+  {
+    cout << str[0];
+  }
+  digitalSub = str.substr(1, str.find("E") - 1);
+  //处理指数部分
+  int pow = 0, base = 1;
+  for (int i = str.length() - 1; i >= 0; i--)
+  {
+    if (str[i] != '+' && str[i] != '-')
+    {
+      pow = pow + (str[i] - '0') * base;
+      base *= 10;
+    }
+    else
+    {
+      if (str[i] == '-')
+      {
+        pow = -pow;
+      }
+      break;
+    }
+  }
+  if (pow == 0)
+  {
+    cout << digitalSub;
+  }
+  else if (pow > 0)
+  {
+    //后移
+    //不需要补0
+    if (pow < (digitalSub.length() - 2))
+    {
+      digitalSub.insert(pow + 2, ".");
+      //删除原有小数点
+      digitalSub.erase(digitalSub.begin() + digitalSub.find("."));
+    }
+    else if (pow == (digitalSub.length() - 2))
+    {
+      //删除原有小数点
+      digitalSub.erase(digitalSub.begin() + digitalSub.find("."));
+    }
+    else
+    {
+      //补0
+      int diff0 = pow - (digitalSub.length() - 2);
+      for (int i = 0; i < diff0; i++)
+      {
+        digitalSub.insert(digitalSub.length(), "0");
+      }
+      //删除原有小数点
+      digitalSub.erase(digitalSub.begin() + digitalSub.find("."));
+    }
+  }
+  else
+  {
+    //将指数变为整数
+    pow = -pow;
+    digitalSub.erase(digitalSub.begin() + 1);
+    for (int i = 0; i < pow; i++)
+    {
+      digitalSub.insert(0, "0");
+    }
+    digitalSub.insert(1, ".");
+  }
+  cout << digitalSub;
+  return 0;
+}
---
+++ PAT1025.cpp
@@ -0,0 +1,35 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: Geeks_Z
+ * @Date: 2021-04-27 20:09:17
+ * @LastEditors: Geeks_Z
+ * @LastEditTime: 2021-04-27 21:12:08
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+  freopen("input.txt", "r", stdin);
+  int first, k, n, nodeId;
+  cin >> first >> n >> k;
+  int data[100005], next[100005], list[100005];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> nodeId;
+    cin >> data[nodeId] >> next[nodeId];
+  }
+  int sum = 0; //不一定所有的输入的结点都是有用的，加个计数器
+  while (first != -1)
+  {
+    list[sum++] = first;
+    first = next[first];
+  }
+  for (int i = 0; i < (sum - sum % k); i += k)
+    reverse(begin(list) + i, begin(list) + i + k);
+  for (int i = 0; i < sum - 1; i++)
+    printf("%05d %d %05d\n", list[i], data[list[i]], list[i + 1]);
+  printf("%05d %d -1", list[sum - 1], data[list[sum - 1]]);
+  return 0;
+}
